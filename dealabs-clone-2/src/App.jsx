c59a390c2f68706703f3cebf677c5ce23b329adf
SuGii <sugii.noob@gmail.com>
2023-10-21 10:28:23 +0200

    first commit

---
+++ dealabs-clone-2/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>New Dealabs Clone modafucka !</h1>
+    </>
+  )
+}
+
+export default App
