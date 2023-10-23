58a92690d1929bf150cf14c85681d9c0878a5f56
SuGii <sugii.noob@gmail.com>
2023-10-23 08:37:58 +0200

    navbar added with some components put in place

--- dealabs-clone-2/src/App.jsx
+++ dealabs-clone-2/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import './App.scss'
+import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
-  
+
   return (
     <>
-      <h1>New Dealabs Clone modafucka !</h1>
+      <header>
+        <Navbar />
+      </header>
     </>
   )
 }
---
+++ dealabs-clone-2/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import "./Navbar.scss"
+import logo from "./img/logo.png"
+
+export default function Navbar(){
+
+
+    return(
+        <>
+            <div className="navbar-container">               
+                <a href="#"><img src={logo} alt="Dealabs-logo" /></a>
+                <ul className="vanish-appear-menu">
+                    <li><a href="#">Menu <span class="material-symbols-outlined">expand_more</span></a></li>
+                </ul>
+                <ul className="vanish-appear-list">
+                    <li><a href="#">Bons plans</a></li>
+                    <li><a href="#">Codes promo<span class="material-symbols-outlined">expand_more</span></a></li>
+                    <li><a href="#">Gratuit</a></li>
+                    <li><a href="#">Discussions</a></li>
+                    <li><a href="#">Plus<span class="material-symbols-outlined">expand_more</span></a></li>                  
+                </ul>
+                <div className="navbar-search">
+                    <span class="material-symbols-outlined">search</span>
+                    <input type="text" placeholder="Recherche..." />
+                </div>
+                <button className="connexion-button"><span class="material-symbols-outlined">person</span>Connexion</button>
+                <button className="post-button"><span class="material-symbols-outlined">add</span>Poster...</button>               
+            </div>               
+        </>
+    )
+}
