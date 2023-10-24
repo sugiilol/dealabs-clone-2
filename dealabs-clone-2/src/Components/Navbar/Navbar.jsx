627f55d76f6a8f8d1f94750aa310d69109387f9a
SuGii <sugii.noob@gmail.com>
2023-10-24 08:38:03 +0200

    navbar - improvement of css and responsiv

--- dealabs-clone-2/src/Components/Navbar/Navbar.jsx
+++ dealabs-clone-2/src/Components/Navbar/Navbar.jsx
@@ -6,24 +6,28 @@ export default function Navbar(){
 
     return(
         <>
-            <div className="navbar-container">               
-                <a href="#"><img src={logo} alt="Dealabs-logo" /></a>
-                <ul className="vanish-appear-menu">
-                    <li><a href="#">Menu <span class="material-symbols-outlined">expand_more</span></a></li>
-                </ul>
-                <ul className="vanish-appear-list">
-                    <li><a href="#">Bons plans</a></li>
-                    <li><a href="#">Codes promo<span class="material-symbols-outlined">expand_more</span></a></li>
-                    <li><a href="#">Gratuit</a></li>
-                    <li><a href="#">Discussions</a></li>
-                    <li><a href="#">Plus<span class="material-symbols-outlined">expand_more</span></a></li>                  
-                </ul>
-                <div className="navbar-search">
-                    <span class="material-symbols-outlined">search</span>
-                    <input type="text" placeholder="Recherche..." />
+            <div className="navbar-container"> 
+                <div className="navbar-left-part">
+                    <a href="#"><img src={logo} alt="Dealabs-logo" /></a>
+                    <ul className="vanish-appear-menu">
+                        <li><a href="#">Menu <span class="material-symbols-outlined">expand_more</span></a></li>
+                    </ul>
+                    <ul className="vanish-appear-list">
+                        <li><a href="#">Bons plans</a></li>
+                        <button>Codes promo<span class="material-symbols-outlined">expand_more</span></button>
+                        <li><a href="#">Gratuit</a></li>
+                        <li><a href="#">Discussions</a></li>                   
+                        <button>Plus<span class="material-symbols-outlined">expand_more</span></button>                 
+                    </ul>
                 </div>
-                <button className="connexion-button"><span class="material-symbols-outlined">person</span>Connexion</button>
-                <button className="post-button"><span class="material-symbols-outlined">add</span>Poster...</button>               
+                <div className="navbar-right-part">
+                    <div className="navbar-search">
+                        <span class="material-symbols-outlined">search</span>
+                        <input type="text" placeholder="Recherche..." />
+                    </div>
+                    <button className="connexion-button"><span class="material-symbols-outlined">person</span>Connexion</button>
+                    <button className="post-button"><span class="material-symbols-outlined">add</span>Poster...</button>
+                </div>                                           
             </div>               
         </>
     )
