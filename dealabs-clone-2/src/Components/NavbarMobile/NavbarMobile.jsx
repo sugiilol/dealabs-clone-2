25961f71ff2ca6a27a5e2ff05096231ce4a3b219
SuGii <sugii.noob@gmail.com>
2023-12-01 08:37:39 +0100

    Navbar Mobile updated with double modal

--- dealabs-clone-2/src/Components/NavbarMobile/NavbarMobile.jsx
+++ dealabs-clone-2/src/Components/NavbarMobile/NavbarMobile.jsx
@@ -2,22 +2,25 @@ import "./NavbarMobile.scss"
 import logo from "./img/logo.png"
 import ModalCustom from "../ModalCustom/ModalCustom"
 import ConnexionModalContent from "../ConnexionModalContent/ConnexionModalContent"
+import DropDownMenuCustom from "../DropDownMenuCustom/DropDownMenuCustom"
+import MenuPromoContent from "../MenuPromoContent/MenuPromoContent"
 
 
 export default function NavbarMobile(){
+
+    const iconBurger = <span className="material-symbols-outlined">menu</span>
     return(
         <>
             <div className="navbar-container-mobile">
                 <div className="navbar-mobile-left-part">
                     <button className="burger-menu-button"><span className="material-symbols-outlined">menu</span></button>
-                    <button className="post-button"><span className="material-symbols-outlined">add</span></button>
+                    <ModalCustom class="post-button" icon="add" content={<ConnexionModalContent />}/>
                 </div>
                 <div className="navbar-mobile-center-part">
                     <a href="#"><img src={logo} alt="Dealabs-logo" /></a>
                 </div>
                 <div className="navbar-mobile-right-part">
                     <button className="search-button"><span className="material-symbols-outlined navbar-search-icon">search</span></button>
-                    {/* <button className="connexion-button"><span className="material-symbols-outlined">person</span></button> */}
                     <ModalCustom class="connexion-button" icon="person" content={<ConnexionModalContent />}/>
                 </div>
             </div>           
